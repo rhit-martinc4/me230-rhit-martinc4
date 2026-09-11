d89984d5ac8ad7dd2746f2532233d3d69517bdad
rhit-martinc4 <martinc4@rose-hulman.edu>
2026-09-10 20:26:20 -0400

    Create Lab_1B.ino

---
+++ Labs/Part1-Electronics/Lab_1B/Lab_1B.ino
@@ -0,0 +1,42 @@
+void setup()
+{
+  pinMode(2, INPUT_PULLUP);
+  pinMode(3, INPUT_PULLUP);
+  
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  if (digitalRead(2) == LOW){
+    digitalWrite(4, HIGH);
+  } else {
+    digitalWrite(4,LOW);
+  }
+  if (digitalRead(3) == LOW){
+    digitalWrite(5, HIGH);
+  } else {
+    digitalWrite(5,LOW);
+  }  
+  if (digitalRead(2) == LOW){
+    digitalWrite(12, HIGH);
+  } else {
+    digitalWrite(12,LOW);
+  }
+  if (digitalRead(3) == LOW){
+    digitalWrite(13, HIGH);
+  } else {
+    digitalWrite(13,LOW);
+  }  
+  analogWrite(6, analogRead(0)/4);
+  analogWrite(11, analogRead(0)/4);
+  
+  delay(10);
+ 
+}
